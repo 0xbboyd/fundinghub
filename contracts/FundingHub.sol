122afe95f33fbd8544ccece028b462e7ee7ed049
Brendan Boyd <firemound@gmail.com>
2016-08-26 14:57:09 -0700

    fixing build errors

--- contracts/FundingHub.sol
+++ contracts/FundingHub.sol
@@ -8,7 +8,7 @@ contract FundingHub {
   }
 
   function createProject(address owner, uint fundGoal, uint deadline) returns (address project){
-    Project newProject = Project(owner, fundGoal, deadline);
+    Project newProject = new Project(owner, fundGoal, deadline);
     address projAddress = newProject.getAddress();
     register[projAddress] = newProject;
     return projAddress;
@@ -18,7 +18,7 @@ contract FundingHub {
     if(msg.value == 0) return false;
     bool success = register[project].fund.value(msg.value);
     if(!success) {
-      msg.sender.send(msg.value);
+      if(!msg.sender.send(msg.value)) throw;
     }
     return success;
   }
